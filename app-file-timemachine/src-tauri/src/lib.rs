627346f51b2c6e08332d8fc32f6bd2e1c48b528c
YuzukoUnderson <rikuichi0212@gmail.com>
2026-05-22 13:51:27 +0900

    [feat] ウィンドウサイズ自動記憶機能の追加とデフォルト解像度の拡張

--- app-file-timemachine/src-tauri/src/lib.rs
+++ app-file-timemachine/src-tauri/src/lib.rs
@@ -26,6 +26,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             greet,
             check_dependencies,
